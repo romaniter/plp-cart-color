79785e4b755ae253cc0afa5a71aef7c56e6049be
Roman <rk-proger@mail.ru>
2019-05-24 23:49:21 +0300

    v1.4
    bugfixes and comments

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 $('.color button').click(function() {
- var target = event.target;
+ //Save button click to variable 
+ var target = event.target; 
+  //Main loop
   while (target.tagName != 'BODY') {
+    //If button check name and cost
+    if ($(target).hasClass('btn')) {
+        text = $(target).find(".name").text();
+        cost = $(target).find(".cost").text();
+    }
+    //If item block change values and break
     if ($(target).attr('data-item')) {
-        $(target).data('item')[0]['value'] = $(target).find(".name").text();
-        if (Boolean($(target).find('.cost').text())) {
-          cost = $(target).find(".cost").text();
+        $(target).data('item')[0]['value'] = text;
+        if (Boolean(cost)) {
           $(target).data('item')[1]['value'] = cost; 
           $(target).find('[data-role="itemprice"] span').text(cost);
         }
         $(target).find('.hoverbtn').removeClass('hoverbtn');
         break;
     }
+    //Nothing to do - go up
     target = target.parentNode;
   }
+ //Reset target for next loop
  target = event.target;
+  //Add border
   while (target.tagName != 'BODY') {
      if ($(target).hasClass('btn')) {
         $(target).addClass('hoverbtn'); 
         return;
     }
- target = target.parentNode;
+    target = target.parentNode;
   }
 });
