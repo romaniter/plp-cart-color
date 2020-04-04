c2bae39d029bda0c54107ba0151cda4a97cc2bf2
Roman <rk-proger@mail.ru>
2020-04-04 20:39:10 +0300

    add photo

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 $('.color button').click(function() {
- //Save button click to variable 
- var target = event.target; 
+ //Save button click to variable
+ var target = event.target;
   //Main loop
   while (target.tagName != 'BODY') {
     //If button check name and cost
     if ($(target).hasClass('btn')) {
         text = $(target).find(".name").text();
         cost = $(target).find(".cost").text();
+        pht = $(target).find(".pht").text();
     }
     //If item block change values and break
     if ($(target).attr('data-item')) {
         $(target).data('item')[0]['value'] = text;
         if (Boolean(cost)) {
-          $(target).data('item')[1]['value'] = cost; 
+          $(target).data('item')[1]['value'] = cost;
           $(target).find('[data-role="itemprice"] span').text(cost);
         }
+       if (Boolean(pht)) {
+       $(target).data('item')[2]['value'] = pht;
+       $(target).find('[data-role="image"] img').attr("src",pht);
+       }
         $(target).find('.hoverbtn').removeClass('hoverbtn');
+         //Add photo to cart
+        function add() {
+	    var grid = $('[data-modal=cart] div:contains("' + text + '")').parents('.grid');
+            var getAtributeImage = $(grid).find('[data-role=itemphoto]').attr('style');
+       	    $(grid).find('[data-role=itemphoto]').attr('style', getAtributeImage +'; ' + 'background-image: url('+ pht +')');
+	};
+
+        $(target).find('[data-action=addtocart]').click(function() {
+               setTimeout(add, 500);
+        });
         break;
     }
     //Nothing to do - go up
@@ -23,10 +38,11 @@ $('.color button').click(function() {
   }
  //Reset target for next loop
  target = event.target;
+
   //Add border
   while (target.tagName != 'BODY') {
      if ($(target).hasClass('btn')) {
-        $(target).addClass('hoverbtn'); 
+        $(target).addClass('hoverbtn');
         return;
     }
     target = target.parentNode;
