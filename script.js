0ddf41dadb191337de3c5901a9c9a92429252472
Roman <rk-proger@mail.ru>
2020-05-10 16:00:24 +0300

    рефакторинг
    Логика немного оптимизирована

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 $('.color button').click(function() {
- //Save button click to variable
  var target = event.target;
-  //Main loop
+  //Основной цикл
   while (target.tagName != 'BODY') {
-    //If button check name and cost
+    //Поиск кнопки и парсинг данных из нее
     if ($(target).hasClass('btn')) {
         text = $(target).find(".name").text();
         cost = $(target).find(".cost").text();
         pht = $(target).find(".pht").text();
     }
-    //If item block change values and break
+    //Если таргет - карточка товара, изменяем данные
     if ($(target).attr('data-item')) {
         $(target).data('item')[0]['value'] = text;
-        if (Boolean(cost)) {
-          $(target).data('item')[1]['value'] = cost;
-          $(target).find('[data-role="itemprice"] span').text(cost);
-        }
-       if (Boolean(pht)) {
-       $(target).data('item')[2]['value'] = pht;
-       $(target).find('[data-role="image"] img').attr("src",pht);
-       }
-        $(target).find('.hoverbtn').removeClass('hoverbtn');
-         //Add photo to cart
-        function add() {
-	    var grid = $('[data-modal=cart] div:contains("' + text + '")').parents('.grid');
-            var getAtributeImage = $(grid).find('[data-role=itemphoto]').attr('style');
-       	    $(grid).find('[data-role=itemphoto]').attr('style', getAtributeImage +'; ' + 'background-image: url('+ pht +')');
-	};
-
-        $(target).find('[data-action=addtocart]').click(function() {
+            //Если есть цена - меняем цену в карточке
+	    if (Boolean(cost)) {
+              $(target).data('item')[1]['value'] = cost;
+              $(target).find('[data-role="itemprice"] span').text(cost);
+            }
+            //Если есть фото - меняем фото в карточке
+	    if (Boolean(pht)) {
+              $(target).data('item')[2]['value'] = pht;
+              $(target).find('[data-role="image"] img').attr("src",pht);
+            }
+	    //Поиск кнопки и удаление обводки
+            $(target).find('.hoverbtn').removeClass('hoverbtn');
+            //Функция добавления картинки к товару в корзине
+	    function add() {
+              var grid = $('[data-modal=cart] div:contains("' + text + '")').parents('.grid');
+              var getAtributeImage = $(grid).find('[data-role=itemphoto]').attr('style');
+              $(grid).find('[data-role=itemphoto]').attr('style', getAtributeImage +'; ' + 'background-image: url('+ pht +')');
+            };
+	    //Установка функции на кнопку по клику
+            $(target).find('[data-action=addtocart]').click(function() {
                setTimeout(add, 500);
-        });
+            });
         break;
     }
-    //Nothing to do - go up
+    //Нечего делать - идем выше
     target = target.parentNode;
   }
- //Reset target for next loop
+ //Сброс таргета для следующего цикла
  target = event.target;
 
-  //Add border
+  //Найти кнопку и добавить обводку
   while (target.tagName != 'BODY') {
      if ($(target).hasClass('btn')) {
         $(target).addClass('hoverbtn');
