1be81bba1a801703926a497f6112c5282bba5ed7
Roman <rk-proger@mail.ru>
2020-08-06 19:11:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $('.color button').click(function() {
             //Если есть цена - меняем цену в карточке
 	    if (Boolean(cost)) {
               $(target).data('item')[1]['value'] = cost;
-              $(target).find('[data-role="itemprice"] span').text(cost);
+              $(target).find('[data-role="itemprice"]>div').children().first().text(cost);
             }
             //Если есть фото - меняем фото в карточке
 	    if (Boolean(pht)) {
